059fb1d54340d3c3f584de55f95e39a80b8ee466
Lewis Crichton <lewi@lewisakura.moe>
2023-03-21 12:15:39 +0000

    flags!

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![deny(clippy::all)]
 use std::{collections::HashMap, sync::Mutex};
 
-use regex::Regex;
+use regex::{Regex, RegexBuilder};
 
 #[macro_use]
 extern crate napi_derive;
@@ -22,7 +22,13 @@ pub fn matches(regex: String, string: String) -> bool {
   let mut cache = REGEX_CACHE.lock().unwrap();
 
   if !cache.contains_key(&regex) {
-    cache.insert(regex.clone(), Regex::new(&regex).unwrap());
+    let reg = RegexBuilder::new(&regex)
+      .multi_line(true)
+      .case_insensitive(true)
+      .build()
+      .unwrap();
+
+    cache.insert(regex.clone(), reg);
   }
 
   cache.get(&regex).unwrap().is_match(&string)
